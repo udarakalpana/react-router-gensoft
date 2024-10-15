fcaad18c790b3cffbbdb4250db87f494fd00dda5
kalpana <kalpanaudara058@gmail.com>
2024-10-15 08:03:09 +0530

    Change to the light mode and add border bottom for navbar.

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,6 +1,6 @@
 const NavBar = () => {
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-white border-gray-200 dark:bg-gray-900 border border-b">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a
           href="https://flowbite.com/"
