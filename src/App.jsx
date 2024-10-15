850414960ea37272b504c13771e8596c4635f431
kalpana <kalpanaudara058@gmail.com>
2024-10-15 07:57:13 +0530

    Add Navigation bar.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import NavBar from "./components/navBar/NavBar.jsx";
+
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+     <NavBar />
     </div>
   );
 };
