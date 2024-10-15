c77e80cb10f17df1af1736c7139d88ec29db89fa
kalpana <kalpanaudara058@gmail.com>
2024-10-15 07:47:41 +0530

    Install prettier package.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 const App = () => {
-
   return (
-      <div><h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1></div>
-  )
-}
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+};
 
-export default App
+export default App;
