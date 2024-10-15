22316279fcd6e6dc5134c59f99e128855d4d8e06
kalpana <kalpanaudara058@gmail.com>
2024-10-15 08:11:15 +0530

    Create home component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import NavBar from "./components/navBar/NavBar.jsx";
+import Home from "./components/home/Home.jsx";
 
 const App = () => {
   return (
     <div>
      <NavBar />
+        <Home />
     </div>
   );
 };
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,11 @@
+import WelcomeSection from "./WelcomeSection.jsx";
+
+const Home = () => {
+  return (
+    <div className="m-4">
+      <WelcomeSection />
+    </div>
+  );
+};
+
+export default Home;
